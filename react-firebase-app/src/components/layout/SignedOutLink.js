e914f43c6d9a4f611ea82b7665665f87b0928595
Fatih Candan <mfatihcandan@gmail.com>
2020-10-13 23:25:17 +0300

    Sign-in & Sign-out components

--- react-firebase-app/src/components/layout/SignedOutLink.js
+++ react-firebase-app/src/components/layout/SignedOutLink.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const SignedOutLinks = () => {
     return (
         <ul className="right">
-            <li><NavLink to='/'>Signupp</NavLink></li>
+            <li><NavLink to='/'>Sign Up</NavLink></li>
             <li><NavLink to='/'>Login</NavLink></li>
         </ul>
     )
