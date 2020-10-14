4b832784dfe4c44bdb16605537e5814ac5fee7e8
Fatih Candan <mfatihcandan@gmail.com>
2020-10-14 20:30:19 +0300

    Adding Redux & Reducers

--- react-firebase-app/src/store/reducers/projectReducer.js
+++ react-firebase-app/src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+    projects: [
+        {id: '1', title: 'Help me', content: 'blah blah blah'},
+        {id: '2', title: 'Collect all stars', content: 'blah blah blah'},
+        {id: '3', title: 'Egg hunt with yoshi', content: 'blah blah blah'}
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
